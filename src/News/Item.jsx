2e55863505a47f1ee63db92ee6381652aa2bef46
wahyudin <49644747+waras47@users.noreply.github.com>
2022-02-27 18:35:44 +0700

    Update Item.jsx

--- src/News/Item.jsx
+++ src/News/Item.jsx
@@ -93,7 +93,7 @@ onChangehandler = (e) => {
                 
                 <Container fluid="md" className='mt-3 ' >
                 <Row md={4}>
-                    {this.news.map((m, i) => 
+                    {this.state.data.map((m, i) => 
                     <Col className='my-2 ' >
                         <Card style={{ width: '15rem' }}>
                         <Card.Img variant="top" src={m.urlToImage} />
